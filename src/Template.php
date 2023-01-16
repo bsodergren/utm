bd94373c5bbe47af9173ce520f593153443fb1b4
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-01-16 04:27:48 +0000

    fist

---
+++ src/Template.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace utm;
+
+class Template
+{
+    public static function sayHello()
+    {
+        echo "Hello World";
+    }
+}
---
+++ src/utm.php
@@ -0,0 +1,6 @@
+<?php
+namespace Bsodergren;
+
+class bsodergren 
+{
+}
