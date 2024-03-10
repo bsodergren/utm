752a393b653bb6b10aa098e6ea82a7198611b5f1
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-03-10 17:28:09 +0000

    ff

---
+++ src/Resources/dump.php
@@ -0,0 +1,82 @@
+<?php 
+
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ use Symfony\Component\VarDumper\Caster\ScalarStub;
+ use Symfony\Component\VarDumper\VarDumper;
+ 
+ function DumpServerExists()
+ {
+     $fp = @fsockopen('tcp://127.0.0.1', 9912, $errno, $errstr, 1);
+     if ($fp) {
+         return true;
+     }
+ 
+     return false;
+ }
+ 
+ if (!function_exists('MyDump')) {
+     /**
+      * @author Nicolas Grekas <p@tchwork.com>
+      * @author Alexandre Daubois <alex.daubois@gmail.com>
+      */
+     function MyDump(mixed ...$vars): mixed
+     {
+         if (false == DumpServerExists()) {
+             return null;
+         }
+ 
+         if (!$vars) {
+             VarDumper::dump(new ScalarStub('🐛'));
+ 
+             return null;
+         }
+ 
+         if (array_key_exists(0, $vars) && 1 === count($vars)) {
+             VarDumper::dump($vars[0]);
+             $k = 0;
+         } else {
+             foreach ($vars as $k => $v) {
+                 VarDumper::dump($v, is_int($k) ? 1 + $k : $k);
+             }
+         }
+ 
+         if (1 < count($vars)) {
+             return $vars;
+         }
+ 
+         return $vars[$k];
+     }
+ }
+ 
+ if (!function_exists('Mydd')) {
+     function Mydd(mixed ...$vars): mixed
+     {
+         if (false == DumpServerExists()) {
+             return null;
+         }
+ 
+         if (!in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) && !headers_sent()) {
+             header('HTTP/1.1 500 Internal Server Error');
+         }
+ 
+         if (array_key_exists(0, $vars) && 1 === count($vars)) {
+             VarDumper::dump($vars[0]);
+         } else {
+             foreach ($vars as $k => $v) {
+                 VarDumper::dump($v, is_int($k) ? 1 + $k : $k);
+             }
+         }
+ 
+         exit(1);
+     }
+ }
+ 
