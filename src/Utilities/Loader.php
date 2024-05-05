5a03e4f18f6a3d330367f9ec8371d230583334b8
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-05-05 14:09:33 -0500

    ddd

---
+++ src/Utilities/Loader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace UTM\Utilities;
+
+use Camoo\Config\Config;
+use Nette\Loaders\RobotLoader;
+
+class Loeader extends RobotLoader
+{
+    public static function loadDatabase($config, $define_pattern, $prefix_pattern, $suffix = '')
+    {
+        $DbConfig = new Config($config);
+
+        foreach ($DbConfig as $key => $data) {
+            $define_name = $prefix_pattern.strtoupper($key.'_prefix').$suffix;
+            define($define_name, $data['prefix']);
+            foreach ($data['tables'] as $k => $t) {
+                $table_def_name = $define_pattern.strtoupper($t).$suffix;
+                define($table_def_name, $data['prefix'].$t);
+            }
+        }
+    }
+}
