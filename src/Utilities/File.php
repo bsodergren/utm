27275aa6c299c30ae2519189d5df4e8ef2ec6b2d
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-05-05 12:48:24 -0500

    dss

---
+++ src/Utilities/File.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace UTM\Utilities;
+
+use Nette\Utils\FileSystem;
+
+class File
+{
+    public static function replace($file, $search, $replacement)
+    {
+        $file = FileSystem::platformSlashes($file);
+
+        $lines = FileSystem::readLines($file);
+        foreach ($lines as $lineNum => $line) {
+            $text[] = str_replace($search, $replacement, $line);
+        }
+        $content = implode("\n", $text);
+        FileSystem::write($file, $content);
+    }
+}
