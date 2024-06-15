e431de1c0bad22894f3de416146813658599aad5
bjorn sodergren <bjorn.sodergren@gmail.com>
2024-06-15 15:34:48 -0500

    vv

--- src/Utilities/Debug/Debug.php
+++ src/Utilities/Debug/Debug.php
@@ -7,16 +7,16 @@ namespace UTM\Utilities\Debug;
 
 use UTM\Bundle\Monolog\UTMLog;
 
-class Debug
+class Debug 
 {
     public $traceStripPrefix = 'ore';
     public static $DebugArray = [];
 
     private static $padding = [
-        'file' => 0,
-        'class' => 0,
-        'function' => 0,
-        'line' => 0,
+        'file' => 20,
+        'class' => 22,
+        'function' => 16,
+        'line' => 4,
     ];
 
     private static $color = [
@@ -26,26 +26,27 @@ class Debug
         'line' => ['green'],
     ];
 
+
+
+
+
+
+    private static function file_append_file($string)
+    {
+        if(is_array($string)) {
+            $string = print_r($string,1);
+        }
+        $file = dirname(realpath($_SERVER['CONTEXT_DOCUMENT_ROOT']), 1)."/tracefile.txt";
+        $fp = fopen($file, 'a+');
+        fwrite($fp, $string);
+        fclose($fp);
+    }
     public static function info($var)
     {
-        // $calling_func = self::tracePath();
-        $class = '';
-        $trace = debug_backtrace();
-        $file = $trace[1]['file'];
-        $line = $trace[1]['line'];
-        $func = '';
-        if (array_key_exists('2', $trace)) {
-            $file = $trace[2]['file'];
-            $line = $trace[2]['line'];
-            if (array_key_exists('class', $trace)) {
-                $class = $trace[2]['class'].'::';
-            }
-            $func = '->'.$class.$trace[2]['function'];
-        }
-        $root = dirname(realpath($_SERVER['CONTEXT_DOCUMENT_ROOT']), 1);
+         $caller = self::tracePath();
 
-        $calling_func = str_replace($root, '', $file).':'.$line.$func;
-        self::$DebugArray[] = ['page' => $calling_func, 'Data' => $var];
+       // $caller = self::CallingFunctionName();
+        self::$DebugArray[] = ['page' => $caller, 'Data' => $var];
     }
 
     public static function ddump()
@@ -63,10 +64,7 @@ class Debug
 
     public static function tracePath()
     {
-        $root = dirname(realpath($_SERVER['CONTEXT_DOCUMENT_ROOT']), 1);
-
         $trace = debug_backtrace();
-
         foreach ($trace as $i => $row) {
             $arg = [];
 
@@ -91,32 +89,25 @@ class Debug
                     }
                     continue;
                 }
-            }
-            // if (str_contains($row['function'], 'require')) {
-            //     continue;
-            // }
-            // if (str_contains($row['function'], 'utminfo')) {
-            //     continue;
-            // }
-            if (\array_key_exists('args', $row)) {
-                $args = $row['args'];
-                if (\is_array($args)) {
-                    foreach ($args as $k => $value) {
-                        if ('' != $value) {
-                            if (\is_array($value)) {
-                                continue;
+                if (\array_key_exists('args', $row)) {
+                    $args = $row['args'];
+                    if (\is_array($args)) {
+                        foreach ($args as $k => $value) {
+                            if ('' != $value) {
+                                if (\is_array($value)) {
+                                    continue;
+                                }
+                                if (\is_object($value)) {
+                                    continue;
+                                }
+                                $arg[] = "'".$value."'";
                             }
-                            if (\is_object($value)) {
-                                continue;
-                            }
-                            $arg[] = "'".$value."'";
                         }
                     }
                 }
+                $arguments = implode(',', $arg);
+                $classArray[] = self::getClassPath($row['class'], 2).':'.$row['function'].'('.$arguments.')';
             }
-            $arguments = implode(',', $arg);
-            $arguments = str_replace($root, '', $arguments);
-            $classArray[] = self::getClassPath($row['class'], 2).':'.$row['function'].'('.$arguments.')';
         }
 
         $classArray = array_reverse($classArray);
@@ -130,17 +121,15 @@ class Debug
             $classPath = str_replace('_', '\\', $classPath);
             if (\is_array($methods)) {
                 $level = 4;
-                $spaces = str_repeat('', $level * 4);
-                $methodPath = implode($spaces.'->', $methods);
+                $spaces = str_repeat(' ', $level * 4);
+                $methodPath = implode("\n".$spaces.'->', $methods);
             }
             $fullPath[] = $classPath.':'.$methodPath;
         }
         $level = 1;
         $spaces = str_repeat(' ', $level * 4);
 
-        $fullPath = str_replace($root, '', $fullPath);
-
-        return implode($spaces.'->', $fullPath);
+        return "\n".implode("\n".$spaces.'->', $fullPath);
     }
 
     public static function CallingFunctionName()
@@ -182,12 +171,10 @@ class Debug
             if (str_contains($row['function'], 'require')) {
                 continue;
             }
-            // if (str_contains($row['function'], 'utminfo')) {
-            //     continue;
-            // }
             if ($row['function']) {
                 $function = self::returnTrace('function', $row);
             }
+
             $TraceList = $calledFile.':'.$class.':'.$function.':'.$calledLine;
             break;
         }
@@ -222,8 +209,7 @@ class Debug
             $text = substr($text, 0, self::$padding[$type]);
             $text = str_pad($text, self::$padding[$type], ' ');
 
-            // return UTMLog::formatPrint($text, self::$color[$type]);
-            return $text;
+            return UTMLog::formatPrint($text, self::$color[$type]);
         }
 
         return null;
