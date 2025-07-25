796fe6832e4f692451ab0364d1500e57070aa401
Bsodergren <bjorn.sodergren@gmail.com>
2025-07-25 21:09:06 +0000

    co

--- src/Utm.php
+++ src/Utm.php
@@ -1,23 +1,24 @@
 <?php
-
 /**
- * Command like Metatag writer for video files.
+ *
+ *   Plexweb
+ *
  */
 
 namespace UTM;
 
 use Dotenv\Dotenv;
 use SaliBhdr\DumpLog\Factory\Logger;
-
-use function defined;
-use function dirname;
+use Camoo\Config\Config;
 
 class Utm
 {
-    public static $SHOW_HTML_DUMP = true;
-    public static $LOG_DIR        = __DIR__.'/logs';
+    public static $SHOW_HTML_DUMP = false;
+    public static $UTM_CONFIG     = [];
+    public static $LOG_DIR        = __DIR__ . '/logs';
     public static $LOG_STYLE      = 'pretty';
     private static $logger;
+
 
     public static $SQL_TABLE_DIR;
     public static $SQL_UPDATE_DIR;
@@ -35,6 +36,12 @@ class Utm
         }
 
         self::$logger = Logger::make(self::$LOG_STYLE)->path(self::$LOG_DIR);
+    }
+
+    public static function loadConifg($file)
+    {
+
+        self::$UTM_CONFIG = (new Config($file))->all();
     }
 
     public static function LoadEnv($directory = '')
@@ -73,16 +80,19 @@ class Utm
         }
     }
 
-    public static function firstRun($dbType = 'mysql', $options = [])
+    public static function firstRun($dbType = 'mysql', $options=[])
     {
         self::$SQL_TABLE_DIR    = $options['table_dir'];
         self::$SQL_UPDATE_DIR   = $options['update_dir'];
         self::$SQL_TABLE_PREFIX = $options['prefix'];
 
-        self::$SQL_USERNAME = $options['username'];
-        self::$SQL_PASSWORD = $options['password'];
-        self::$SQL_HOSTNAME = $options['hostname'];
-        self::$SQL_DATABASE = $options['database'];
+        self::$SQL_USERNAME     = $options['username'];
+        self::$SQL_PASSWORD     = $options['password'];
+        self::$SQL_HOSTNAME     = $options['hostname'];
+        self::$SQL_DATABASE     = $options['database'];
+
+
+
     }
 }
 // require_once __DIR__.'/Resources/Options.php';
